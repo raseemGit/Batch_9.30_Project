6aaa6c5be5b433c02cb50ca3c899df31d79171fa
raseemGit <raseem.moha@gmail.com>
2019-12-04 10:16:22 +0530

    MyFirstCode

---
+++ src/com/hexaware/SampleClass.java
@@ -0,0 +1,24 @@
+package com.hexaware;
+
+public class SampleClass {
+
+	private void method1() {
+
+		System.out.println("java classs");
+
+	}
+
+	private void method2() {
+		System.out.println("Selenium class");
+	}
+	
+	
+	public static void main(String[] args) {
+		SampleClass obj = new SampleClass();
+		
+		obj.method1();
+		obj.method2();
+		
+	}
+
+}
